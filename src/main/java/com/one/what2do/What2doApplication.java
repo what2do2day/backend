092478503c5d724161d81afac53cab155c7c3c7c
NamJihyun99 <namjihyun9936@gmail.com>
2025-06-30 17:13:48 +0900

    init

---
+++ src/main/java/com/one/what2do/What2doApplication.java
@@ -0,0 +1,13 @@
+package com.one.what2do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What2doApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(What2doApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/what2do/What2doApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.what2do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What2doApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
